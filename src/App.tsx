df66abeb2136f2d6932fafd00e485bccfcb3be28
Sebastian (seprog) Poppenborg <sebastianpoppenborg@gmail.com>
2025-10-13 01:31:16 +0200

    refactored Wordle component into own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import random from 'random'
-import { useState } from 'react'
 
+import { Wordle } from './Wordle'
 import wordles from './wordles.yaml'
 import './index.css'
 
@@ -17,83 +17,6 @@ export function App() {
         ) ?? random.int(0, wordles.length)
       ))
     ]} />
-  )
-}
-
-function Wordle({ wordle }: {
-  wordle: {
-    [solution: string]: string[]
-  }
-}) {
-  const solution = Object.keys(wordle).pop()!
-  const hints = wordle[solution]!
-
-  const [ guesses, setGuesses ] = useState<string[]>([])
-  function makeGuess(guess: string) {
-    if (guess && guess.length === solution.length) setGuesses((guesses) => [
-      ...guesses,
-      guess
-    ])
-  }
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Round</th>
-          <th>Guess</th>
-          <th>Hint</th>
-        </tr>
-      </thead>
-      <tbody>
-        { hints.slice(0, guesses.length+1).map((hint, round) => (
-          <tr key={round}>
-            <td>{ round + 1 }</td>
-            <td>{
-              guesses[round]
-              ? <FormattedGuess guess={guesses[round]} solution={solution} />
-              : <GuessForm makeGuess={makeGuess} solutionLength={solution.length} />
-            }</td>
-            <td>{ hint }</td>
-          </tr>
-        )) }
-      </tbody>
-    </table>
-  )
-}
-
-function FormattedGuess({ guess, solution }: {
-  guess: string,
-  solution: string
-}) {
-  return (
-    guess.toUpperCase().split('').map((c, n) => (
-      <span
-        key={n}
-        className={
-          solution.toUpperCase().includes(c)
-          ? c === solution.toUpperCase().at(n)
-          ? 'text-green-700'
-          : 'text-yellow-700'
-          : 'text-red-700'
-        }
-      >{ c }</span>
-    ))
-  )
-}
-
-function GuessForm({ makeGuess, solutionLength }: {
-  makeGuess: (guess: string) => void
-  solutionLength: number
-}) {
-  return (
-    <form onSubmit={(e) => {
-      e.preventDefault()
-      makeGuess(new FormData(e.currentTarget).get('guess') as string | null ?? '')
-    }}>
-      <input type='text' name='guess' />
-      <input type='submit' value={`Submit (${solutionLength})`} />
-    </form>
   )
 }
 
